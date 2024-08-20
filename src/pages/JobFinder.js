3c18ece27b240a96bea12b583a29aa05c37c5711
RusiruD <126373366+RusiruD@users.noreply.github.com>
2024-08-20 13:08:01 +1200

    feat: Added higher resolution images, including ai upscaling and an attribution file (#51)

--- src/pages/JobFinder.js
+++ src/pages/JobFinder.js
@@ -12,22 +12,22 @@ function JobFinder() {
       <div className="row justify-content-center">
         <div className="col-md-4 mb-4">
           <a href="https://nz.prosple.com/search-jobs?content=internships-new-zealand&study_fields=502%2C502%7C510&locations=796&defaults_applied=1&start=0&opportunity_types=2" target="_blank" rel="noopener noreferrer">
-            <div className="card h-100 border border-dark">
+            <div className="card h-100 border border-dark d-flex justify-content-center">
               <img src="images/prosple.png" className="card-img-top p-3" alt="Prosple" />
             </div>
           </a>
         </div>
         <div className="col-md-4 mb-4">
           <a href="https://app.summeroftech.co.nz/jobs" target="_blank" rel="noopener noreferrer">
-            <div className="card h-100 border border-dark">
-              <img src="images/summer-of-tech.png" className="card-img-top p-3" alt="Summer of Tech" />
+            <div className="card h-100 border border-dark d-flex justify-content-center">
+              <img src="images/summer_of_tech.png" className="card-img-top p-3" alt="Summer of Tech" />
             </div>
           </a>
         </div>
-        <div className="col-md-4 mb-4">
+        <div className="col-md-4 mb-4 ">
           <a href="https://www.seek.co.nz/jobs-in-information-communication-technology/in-All-Auckland" target="_blank" rel="noopener noreferrer">
-            <div className="card h-100 border border-dark">
-              <img src="images/seek.png" className="card-img-top p-3" alt="Seek" />
+            <div className="card h-100 border border-dark d-flex justify-content-center">
+              <img src="images/seek.png" className="card-img-top p-3"  alt="Seek" />
             </div>
           </a>
         </div>
@@ -36,21 +36,21 @@ function JobFinder() {
       <div className="row justify-content-center">
         <div className="col-md-4 mb-4">
           <a href="https://nz.indeed.com/jobs?q=software+%2C+intern&l=New+Zealand&from=searchOnDesktopSerp&vjk=78107bef7ff5927f" target="_blank" rel="noopener noreferrer">
-            <div className="card h-100 border border-dark">
+            <div className="card h-100 border border-dark d-flex justify-content-center">
               <img src="images/indeed.png" className="card-img-top p-3" alt="Indeed" />
             </div>
           </a>
         </div>
         <div className="col-md-4 mb-4">
           <a href="https://www.glassdoor.co.nz/Job/new-zealand-software-intern-jobs-SRCH_IL.0,11_IN186_KO12,27.htm" target="_blank" rel="noopener noreferrer">
-            <div className="card h-100 border border-dark">
+            <div className="card h-100 border border-dark d-flex justify-content-center">
               <img src="images/glassdoor.png" className="card-img-top p-3" alt="Glassdoor" />
             </div>
           </a>
         </div>
         <div className="col-md-4 mb-4">
           <a href="https://www.linkedin.com/jobs/search/?currentJobId=3935175062&geoId=105490917&keywords=software%20internship&origin=JOB_SEARCH_PAGE_SEARCH_BUTTON&originalSubdomain=nz&refresh=true" target="_blank" rel="noopener noreferrer">
-            <div className="card h-100 border border-dark">
+            <div className="card h-100 border border-dark d-flex justify-content-center">
               <img src="images/linkedin.png" className="card-img-top p-3" alt="LinkedIn" />
             </div>
           </a>
